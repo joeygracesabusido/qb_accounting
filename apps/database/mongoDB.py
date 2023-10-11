4abcbed9971a0909eee55ac491484e67d7580085
joeygracesabusido <joeygracesabusido@gmail.com>
2023-10-11 13:34:09 +0800

    testing

--- apps/database/mongoDB.py
+++ apps/database/mongoDB.py
@@ -2,6 +2,8 @@ from pymongo import MongoClient
 import pymongo
 
 
+
+
 def create_mongo_client():
     var_url = f"mongodb+srv://joeysabusido:genesis11@cluster0.r76lv.mongodb.net/accounting_database?retryWrites=true&w=majority"
     client = MongoClient(var_url, maxPoolSize=None)
--- apps/routes/login.py
+++ apps/routes/login.py
@@ -4,10 +4,34 @@ from fastapi.responses import HTMLResponse
 from typing import Union, List
 
 
+from apps.authentication.utils import OAuth2PasswordBearerWithCookie
+
+from jose import jwt
+
+JWT_SECRET = 'myjwtsecret'
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+employee_user = APIRouter(include_in_schema=False)
+templates = Jinja2Templates(directory="templates")
+
+
+
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+
+oauth_scheme = OAuth2PasswordBearerWithCookie(tokenUrl="token")
+
+
+
+from passlib.context import CryptContext
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+
 login_router = APIRouter(include_in_schema=False)
 templates = Jinja2Templates(directory="templates")
 
 
 @login_router.get("/")
 def read_root():
-    return {"Hello": "World!!!!!"}
+    return {"Hello": "World"}
