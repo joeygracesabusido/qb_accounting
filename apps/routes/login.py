c9b755fbeb41f1956493c3cd70bdb817ace08d9e
unknown <joeysabusido.@gmail.com>
2023-10-12 07:15:52 +0300

    10.12.2023

--- apps/routes/login.py
+++ apps/routes/login.py
@@ -1,37 +1,146 @@
 from fastapi import APIRouter, Body, HTTPException, Depends, Request, Response, status
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
-from typing import Union, List
+from typing import Union, List, Optional
+from datetime import timedelta
+from datetime import datetime, date
 
 
 from apps.authentication.utils import OAuth2PasswordBearerWithCookie
 
+from apps.database.mongoDB import create_mongo_client
+mydb = create_mongo_client()
+
 from jose import jwt
 
+SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
 JWT_SECRET = 'myjwtsecret'
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
-employee_user = APIRouter(include_in_schema=False)
+login_router = APIRouter()
 templates = Jinja2Templates(directory="templates")
-
-
-
-from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
-
-oauth_scheme = OAuth2PasswordBearerWithCookie(tokenUrl="token")
 
 
 
 from passlib.context import CryptContext
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm,SecurityScopes
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
+oauth_scheme = OAuth2PasswordBearerWithCookie(tokenUrl="token")
+# oauth_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
-login_router = APIRouter(include_in_schema=False)
-templates = Jinja2Templates(directory="templates")
+def get_password_hash(password):
+    return pwd_context.hash(password)
+
+def create_access_token(data: dict, expires_delta: timedelta):
+    to_encode = data.copy()
+
+    expire = datetime.utcnow() + expires_delta
+
+    to_encode.update({"exp": expire})
+
+    
+    return to_encode
+@login_router.post('/sign-up')
+def sign_up(fullname: str, username: str, password: str, status: str,
+            role: str,created: Union[datetime, None] = Body(default=None)):
+    """This function is for inserting """
+    dataInsert = dict()
+    dataInsert = {
+        "fullname": fullname,
+        "username": username,
+        "password": get_password_hash(password),
+        "status": status,
+        "role": role,
+        "created": created
+        
+        }
+    mydb.login.insert_one(dataInsert)
+    return {"message":"User has been save"} 
+
+def authenticate_user(username, password):
+    user = mydb.login.find({"username":username})
+
+    for i in user:
+        username = i['username']
+        password1 = i['password']
+   
+        if user:
+            password_check = pwd_context.verify(password,password1)
+            return password_check
+
+        else :
+            False
+
+
+@login_router.get('/api-login/')
+def login(username1: Optional[str],password1:Optional[str],response:Response):
+    username = username1
+    password = password1
+
+
+    user = authenticate_user(username,password)
+    if not user:
+        raise HTTPException(status_code=400, detail="Incorrect username or password")
+       
+
+    access_token = create_access_token(
+                data = {"sub": username,"exp":datetime.utcnow() + timedelta(ACCESS_TOKEN_EXPIRE_MINUTES)}, 
+                expires_delta=timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+                                    )
+
+    data = {"sub": username,"exp":datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)}
+    jwt_token = jwt.encode(data,SECRET_KEY,algorithm=ALGORITHM)
+    response.set_cookie(key="access_token", value=f'Bearer {jwt_token}',httponly=True)
+    # return response
+    
+    return {"access_token": jwt_token, "token_type": "bearer"}
+
+
+# how to get the user through authentication
+@login_router.get("/current-user/")
+async def get_current_user(request:Request):
+
+    try :
+        token = request.cookies.get('access_token')
+        
+        # print(token)
+        if token is None:
+            raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail= "Not Authorized",
+            # headers={"WWW-Authenticate": "Basic"},
+            )
+        else:
+            scheme, _, param = token.partition(" ")
+            payload = jwt.decode(param, SECRET_KEY, algorithms=ALGORITHM)
+        
+            username = payload.get("sub")    
+            
+            expiration_time = datetime.fromtimestamp(payload.get("exp"))
+            # print(expiration_time)
+            # print(datetime.utcnow())
+
+            
+            if datetime.utcnow() > expiration_time:
+                raise HTTPException(
+                    status_code=status.HTTP_401_UNAUTHORIZED,
+                    detail="Token has expired. Please login again.",
+                )
+
+            # response_data = {"username": username}
+            return username
+
+    except Exception as e:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail= "Session has expired",
+            # headers={"WWW-Authenticate": "Basic"},
+        )
 
 
 @login_router.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return {"Hello": "World!!!"}
